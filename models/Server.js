cbf1339566791abe8d44fababcf42ae41b173ef6
Paweł Kapała <bylek77@gmail.com>
2017-03-27 19:25:21 +0200

    Server password & key encryption

--- models/Server.js
+++ models/Server.js
@@ -1,5 +1,8 @@
+const { algorithm, salt } = require('config').encryption;
+const crypto = require('crypto');
+
 module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('Server', {
+  const Server = sequelize.define('Server', {
     name: DataTypes.TEXT,
     description: DataTypes.TEXT,
     address: DataTypes.TEXT,
@@ -9,11 +12,56 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.ENUM,
       values: ['KEY', 'PASSWORD']
     },
-    key: DataTypes.TEXT,
-    password: DataTypes.TEXT
+    key_digest: DataTypes.TEXT,
+    key: DataTypes.VIRTUAL,
+    password_digest: DataTypes.STRING,
+    password: DataTypes.VIRTUAL
   }, {
     tableName: 'servers',
     underscored: true,
-    timestamps: false
+    timestamps: false,
+    instanceMethods: {
+      getPassword: function(){
+        return decrypt(this.password_digest);
+      },
+      getKey: function(){
+        return decrypt(this.key_digest);
+      }
+    }
   });
+
+  Server.beforeCreate(encryptData);
+  Server.beforeUpdate(encryptData);
+
+  return Server;
 };
+
+async function encryptData(server, options, callback){
+  if (server.get('password')) {
+    let encryptedPassword = encrypt(server.get('password'));
+    server.set('password_digest', encryptedPassword);
+  }
+
+  if (server.get('key')) {
+    let encryptedKey = encrypt(server.get('key'));
+    server.set('key_digest', encryptedKey);
+  }
+
+  callback(null, options);
+}
+
+function encrypt(text){
+  let cipher = crypto.createCipher(algorithm, salt);
+  let  crypted = cipher.update(text, 'utf8', 'hex');
+  crypted += cipher.final('hex');
+
+  return crypted;
+}
+
+function decrypt(text){
+  let decipher = crypto.createDecipher(algorithm, salt);
+  let dec = decipher.update(text,'hex','utf8');
+  dec += decipher.final('utf8');
+
+  return dec;
+}
