1e6d2b74dd703a8f3c833f7de0b687aaaee85465
Paweł Kapała <bylek77@gmail.com>
2017-10-17 16:35:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-// initialize sequelize models
+// initialize sequelize models 
 const models = require('./models');
 const sequelize = models.sequelize;
 app.set('models', models);
